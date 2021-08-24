ae48bc934d2f69d1fdd1bb3533a645c910b10255
Vaishonline <vaishnavipraveen2000@gmail.com>
2021-08-24 16:17:43 +0530

    tenancy-for-laravel jetstream inertia-teams

---
+++ app/Models/Tenant.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
+use Stancl\Tenancy\Contracts\TenantWithDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDomains;
+use Stancl\Tenancy\Database\Concerns\HasDatabase;
+
+class Tenant extends BaseTenant implements TenantWithDatabase
+{
+    use HasFactory, HasDatabase, HasDomains;
+    protected $fillable = ['id'];
+}
